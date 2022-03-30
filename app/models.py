d4e0332873153224eb3bdc8e2b60cb1379f8c4e0
Katelyn Mehner <kmehner@gmail.com>
2022-03-30 17:36:17 -0600

    The basics

---
+++ app/models.py
@@ -0,0 +1,59 @@
+from app import db 
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User|{self.username}>"
+
+    def __str__(self):
+        return self.username
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), unique=True, nullable=False)
+    body = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Post|{self.title}>"
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.Integer, unique = True, nullable = False)
+    address = db.Column(db.Text(200), nullable=False)
+    date_created = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Address {self.id} |{self.first_name} {self.last_name}>"
+
+    def __str__(self):
+        return f"""
+        Name: {self.first_name} {self.last_name}
+        Phone: {self.phone_number}
+        Address: {self.address}
+        """
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,47 @@
 from app import app
-from flask import render_template
+from flask import redirect, render_template, url_for
+from app.forms import AddressForm, SignUpForm
+from app.models import User, Address, Post
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   title = "Home"
+   return render_template('index.html', title=title)
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    title = 'Sign Up'
+    form = SignUpForm()
+    # check if a post request and that the form is valid
+    if form.validate_on_submit():
+        # Get data from the validated form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        # Create a new user instance with form data
+        new_user = User(email=email, username=username, password=password)
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', title=title, form=form)
+
+
+@app.route('/login')
+def login():
+    title = 'Log In'
+    return render_template('login.html', title=title)
+
+@app.route('/register_phone_number', methods=['GET', 'POST'])
+def register_phone_number():
+   title = "Register Phone Number"
+   form = AddressForm()
+   addresses = Address.query.all()
+   if form.validate_on_submit():
+      first_name = form.first_name.data
+      last_name = form.last_name.data
+      phone = form.phone_number.data
+      address = form.address.data
+      Address(first_name=first_name, last_name=last_name, address=address, phone_number=phone)
+      return redirect(url_for('index'))
+
+
+   return render_template('register_address.html', title=title, form=form)
